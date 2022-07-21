bb8d9fe8302286159576055a2d753ab150271cec
Hannah Peterson <hannah.peterson360@gmail.com>
2022-07-20 20:49:35 -0700

    Add functions to prompt or reprompt users until they enter rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -1,22 +1,31 @@
-// the user enters either rock, paper, or scissors 
-let userChoice = prompt("Please select rock, paper, or scissors.");
-// if the user enters any other choice, an error is displayed
-
-if (userChoice ===("scissors")){
-    ("");
-} else if (userChoice===("rock")){
-    ("");
-} else if (userChoice===("paper")){
-    ("");
-} else {
-    alert("Error")
-};
-console.log (userChoice);
-
-// the computer randomly selects either rock, paper, 
-//or scissors 
-
 let options = ["rock", "paper", "scissors"];
+let userChoice;
+
+//prompt user to enter rock paper or scissors
+function getUserChoice (){
+    userChoice = prompt("Please select rock, paper, or scissors.");
+//    if (userChoice == "rock" || "paper" || "scissors" || userError ());
+    if (userChoice === ("scissors"))
+        (userChoice = "scissors");
+    else if (userChoice === ("rock")) 
+        userChoice = ("rock");
+    else if (userChoice === ("paper")) 
+        userChoice = ("paper");
+        else { userError()};
+    return userChoice;
+    }
+//re-prompt user if input does not equal rock paper or scissors
+function userError (){
+    if (options.includes(userChoice) == true){
+    ("");}
+    else {getUserChoice();
+    };
+} 
+
+console.log(getUserChoice ());
+
+// the computer randomly selects either rock paper or scissors 
+
 let computerChoice;
 
 function getComputerChoice (){
@@ -26,14 +35,24 @@ return computerChoice;
 }
 console.log(getComputerChoice ());
 
-// compare the values the user entered and the computer chose 
-//and determine a winner assuming paper beats rock, 
-//rock beats scissors, scissors beats paper 
-
+// compare the values the user entered and the computer chose and determine a winner assuming paper beats rock, 
+//rock beats scissors, scissors beats paper if computer wins, display message "You Lost! "
+if (userChoice == "rock" && computerChoice == "paper") {
+    alert("You Lost!");
+} else if (userChoice == "paper" && computerChoice == "scissors") {
+    alert ("You Lost!");
+} else if (userChoice == "scissors" && computerChoice == "rock"){
+    alert ("You Lost!");
 // if the computers choice and users choice are the same, display message "You tied"
-if (userChoice == computerChoice) {
+} else if (userChoice == computerChoice) {
     alert("You Tied!");
-}
-// if computer wins, display message "Sorry, you lost"
+} 
 // if user wins, display message "You won!"
+ else {
+    alert ("You Won!");
+}
+
+
+
+
 
