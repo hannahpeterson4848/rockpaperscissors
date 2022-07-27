ddd599b38b49e0c7b705717ee5861ca01cbece55
Hannah Peterson <hannah.peterson360@gmail.com>
2022-07-26 21:12:19 -0700

    Print user and computers choice (r, p or s) to the console for 5 rounds (1 game)

--- rps.js
+++ rps.js
@@ -5,7 +5,7 @@ let userChoice;
 function getUserChoice (){
     userChoice = prompt("Please select rock, paper, or scissors.");
 //if (userChoice == "rock" || "paper" || "scissors" || userError ());
-// let user choice be case insensitive
+//let user choice be case insensitive
     userChoice = userChoice.toLowerCase();
     if (userChoice === ("scissors"))
         (userChoice = "scissors");
@@ -16,14 +16,13 @@ function getUserChoice (){
         else { userError()};
     return userChoice;
     }
-//re-prompt user if input does not equal rock paper or scissors
+//create a function to re-prompt user if input does not equal rock paper or scissors
 function userError (){
     if (options.includes(userChoice) == true){
     ("");}
     else {getUserChoice();
     };
 } 
-//console.log(getUserChoice ());
 // the computer randomly selects either rock paper or scissors 
 let computerChoice;
 
@@ -32,10 +31,13 @@ let randomOption = Math.floor(Math.random()*options.length);
 computerChoice = options[randomOption];
 return computerChoice;
 }
-//console.log(getComputerChoice ());
 // compare the values the user entered and the computer chose and determine a winner assuming paper beats rock, rock beats scissors, scissors beats paper 
 // create a function to play one round of the game
-function playRound (computerChoice, userChoice){
+function playRound (){
+    getComputerChoice ();
+    getUserChoice();
+    console.log(computerChoice);
+    console.log(userChoice);
     if (userChoice == "rock" && computerChoice == "paper") {
         return ("You Lost!");
     } else if (userChoice == "paper" && computerChoice == "scissors") {
@@ -48,26 +50,17 @@ function playRound (computerChoice, userChoice){
         return ("You Won!");
     }
 }
-
-//console.log(playRound(computerChoice, userChoice));
-//create a variable to store the results of 1 round of rock paper scissors
-let roundResults;
-//create a function that plays 5 rounds of rock paper scissors
+//create a variable to store the results of 1 round 
+let gameResults;
+//create a function that plays 5 rounds 
 function game (){
     for (let i = 0; i < 5; i++) {
-    //call playRound function with new computerChoices and userChoices each iteration
-    getComputerChoice ();
-    getUserChoice ();
-    //record the results in a var each time a round is played
-    roundResults = playRound ();
-    //return the results of each round to the console
-    console.log(roundResults);    
+    //call playRound function 5 times
+    gameResults = playRound ();
     }
 }
-//play a game of rock paper scissors
+//play a game 
 game ();
-
-
 
 
 
