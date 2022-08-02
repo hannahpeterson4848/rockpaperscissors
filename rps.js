8571dc99235e310fbcf24a489ae01755b4853752
Hannah Peterson <hannah.peterson360@gmail.com>
2022-08-01 20:22:15 -0700

    Clean up code by editing comments and deleting unused indents

--- rps.js
+++ rps.js
@@ -1,10 +1,9 @@
 //create a variable for the intended user input
 let options = ["rock", "paper", "scissors"];
 let userChoice;
-//prompt user to enter rock paper or scissors
+//create a function to prompt user to enter rock paper or scissors
 function getUserChoice (){
     userChoice = prompt("Please select rock, paper, or scissors.");
-//if (userChoice == "rock" || "paper" || "scissors" || userError ());
 //let user choice be case insensitive
     userChoice = userChoice.toLowerCase();
     if (userChoice === ("scissors"))
@@ -23,20 +22,19 @@ function userError (){
     else {getUserChoice();
     };
 } 
-// the computer randomly selects either rock paper or scissors 
+// create a function where the computer randomly selects either rock paper or scissors 
 let computerChoice;
-
 function getComputerChoice (){
-let randomOption = Math.floor(Math.random()*options.length);
-computerChoice = options[randomOption];
-return computerChoice;
+    let randomOption = Math.floor(Math.random()*options.length);
+    computerChoice = options[randomOption];
+    return computerChoice;
 }
-// compare the values the user entered and the computer chose and determine a winner assuming paper beats rock, rock beats scissors, scissors beats paper 
+// create a variable to track the outcome of a round
 let roundOutcome;
-// create an variable to track user and computer score
+// create variables to track user and computer score
 let userScore = 0;
 let computerScore=0;
-// create a function to play one round of the game
+// create a function to play one round of rock, paper, scissors
 function playRound (){
     getComputerChoice ();
     getUserChoice();
@@ -67,10 +65,9 @@ function playRound (){
         console.log (roundOutcome);
     }
 }
-//create a variable to store the results of 1 round 
 let gameResults;
-//create a function that plays 5 rounds 
-function game (){
+//create a function that plays 5 rounds of rock paper scissors
+function playGame (){
     for (let i = 0; i < 5; i++) {
     //call playRound function 5 times
     gameResults = playRound ();
@@ -83,10 +80,9 @@ function game (){
     } else {
         console.log("You lost the game.")
     }
-
 }
 //play a game 
-game ();
+playGame ();
 
 
 
