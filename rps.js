5274c17595e59bfe8cd3a43dceb57efdbc4a8376
Hannah Peterson <100668682+hannahpeterson4848@users.noreply.github.com>
2022-08-24 20:25:34 -0700

    Create and layout divs to show computer and user scores

--- rps.js
+++ rps.js
@@ -1,55 +1,25 @@
 //create a variable for the intended user input
 let options = ["rock", "paper", "scissors"];
 let userChoice; 
-
-const btnRock = document.querySelector(".button-rock");
-const btnPaper = document.querySelector(".button-paper");
-const btnScissors = document.querySelector(".button-scissors");
-
-btnRock.addEventListener ('click', function (){
-    userChoice = "rock";
-});
-btnPaper.addEventListener ('click', function (){
-    userChoice = "paper";
-});
-btnScissors.addEventListener ('click', function (){
-    userChoice = "scissors";
-});
-//create a function to prompt user to enter rock paper or scissors
-//function getUserChoice (){
-  //  userChoice = prompt("Please select rock, paper, or scissors.");
-    //let user choice be case insensitive 
-    //userChoice = userChoice.toLowerCase();
-    //if (userChoice === ("scissors"))
-      //  (userChoice = "scissors");
-    //else if (userChoice === ("rock")) 
-      //  userChoice = ("rock");
-    //else if (userChoice === ("paper")) 
-      //  userChoice = ("paper");
-        //else { userError()};
-    //return userChoice;
-    //}
-//create a function to re-prompt user if input does not equal rock paper or scissors
-//function userError (){
-  //  if (options.includes(userChoice) == true){
-    //("");}
-    //else {getUserChoice();
-    //};
-//} 
-// create a function where the computer randomly selects either rock paper or scissors 
-let computerChoice;
-function getComputerChoice (){
-    let randomOption = Math.floor(Math.random()*options.length);
-    computerChoice = options[randomOption];
-    return computerChoice;
-}
 // create a variable to track the outcome of a round
 let roundOutcome;
 // create variables to track user and computer score
 let userScore = 0;
 let computerScore=0;
-// create a function to play one round of rock, paper, scissors
-function playRound (){
+
+const btnRock = document.querySelector(".button-rock");
+const btnPaper = document.querySelector(".button-paper");
+const btnScissors = document.querySelector(".button-scissors");
+
+let computerChoice;
+
+function getComputerChoice (){
+    let randomOption = Math.floor(Math.random()*options.length);
+    computerChoice = options[randomOption];
+    return computerChoice;
+}
+
+function playRound (userChoice){
     getComputerChoice ();
     console.log(computerChoice);
     console.log(userChoice);
@@ -57,28 +27,54 @@ function playRound (){
         roundOutcome = ("You Lost!");
         userScore --;
         computerScore ++;
-        console.log (roundOutcome);
+        alert (roundOutcome);
     } else if (userChoice == "paper" && computerChoice == "scissors") {
         roundOutcome = ("You Lost!");
         userScore --;
         computerScore ++;
-        console.log (roundOutcome);
+        alert (roundOutcome);
     } else if (userChoice == "scissors" && computerChoice == "rock"){
         roundOutcome = ("You Lost!");
         userScore --;
         computerScore ++;
-        console.log (roundOutcome);
+        alert (roundOutcome);
     } else if (userChoice == computerChoice) {
         roundOutcome = ("You Tied!");
-        console.log (roundOutcome);
+        alert (roundOutcome);
     }  else {
         roundOutcome = ("You Won!");
         userScore ++;
         computerScore --;
-        console.log (roundOutcome);
+        alert (roundOutcome);
     }
 }
-console.log (playRound());
+btnRock.addEventListener ('click', function () {
+    playRound("rock");
+});
+
+btnPaper.addEventListener ('click', function () {
+    playRound("paper");
+});
+
+btnScissors.addEventListener ('click', function () {
+    playRound("scissors");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create a function to play one round of rock, paper, scissors
+
 //let gameResults;
 //create a function that plays 5 rounds of rock paper scissors
 //function playGame (){
